1cdae6e4a6c11d288447c43a098e46eab3916fec
GledfullyChill <stephan.lofstrom@gmail.com>
2021-08-10 17:11:15 -0600

    add many comments, updated count function to "best practice"

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
-  $('textarea').on('input', () => {
-    let $counter = 140;
-    $counter -= $('textarea').val().length;
-    $('.counter').val($counter);
+  //on input function to reduce counter based on the amount of characters in the text field
+  $('textarea').on('input', function() {
+    let sum = 140;
+    //assign the counter to a variable
+    let $counter = $(this).parent().find('.counter');
+    //reduce the text field current chracter length
+    sum -= $(this).val().length;
+    //push the sum into the value of the counter
+    $counter.val(sum);
+    //if the value of the counter is less than 0 turn the text red, return it to base color if it goes back to 0 or more
+    if (sum < 0) {
+      $counter.addClass('negNum')
+    }
+    if (sum >= 0) {
+      $counter.removeClass('negNum')
+    }
   });
 
 })
